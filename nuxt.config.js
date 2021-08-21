2b3a1e9ad9226d9ec5a16e25d1974bf3926e5540
Adefowowe <aadebamowo@ifriqiyacapital.com>
2021-08-21 18:34:34 +0100

    config update to reference project source folder as 'src'

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  srcDir: 'src/',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'learning-app-spa',
